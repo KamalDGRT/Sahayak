7a4c09626062744c132c6519e5adead9e79fffdc
KamalDGRT <kamaldgrt@gmail.com>
2024-08-11 14:59:46 +0530

    fresh framework setup

---
+++ Sahayak/Sahayak.h
@@ -0,0 +1,18 @@
+//
+//  Sahayak.h
+//  Sahayak
+//
+//  Created by Kamal Sharma on 10/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sahayak.
+FOUNDATION_EXPORT double SahayakVersionNumber;
+
+//! Project version string for Sahayak.
+FOUNDATION_EXPORT const unsigned char SahayakVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sahayak/PublicHeader.h>
+
+
