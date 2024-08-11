fe9d948b2b174be3e6fb24cce20d21d85844f38c
KamalDGRT <kamaldgrt@gmail.com>
2024-08-11 17:39:53 +0530

    ft: add ui toggle styles

---
+++ Sahayak/UI/ToggleStyles.swift
@@ -0,0 +1,72 @@
+//
+//  ToggleStyles.swift
+//  Sahayak
+//
+//  Created by Kamal Sharma on 11/08/24.
+//
+
+import SwiftUI
+
+struct CheckmarkToggleStyle: ToggleStyle {
+    var onColor = Color.green
+    var offColor = Color.gray
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }) {
+            Rectangle()
+                .foregroundColor(configuration.isOn ? onColor : offColor)
+                .frame(width: 51, height: 31, alignment: .center)
+                .overlay(
+                    circle(configuration.$isOn)
+                )
+                .cornerRadius(25)
+        }
+    }
+    
+    func circle(_ isOn: Binding<Bool>) -> some View {
+        Circle()
+            .foregroundColor(.white)
+            .padding(.all, 3)
+            .overlay(
+                Image(systemName: isOn.wrappedValue ? "checkmark" : "xmark")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .font(Font.title.weight(.black))
+                    .frame(width: 8.0, height: 8.0, alignment: .center)
+                    .foregroundColor(isOn.wrappedValue ? .green : .gray)
+            )
+            .offset(x: isOn.wrappedValue ? 11 : -11, y: 0)
+    }
+}
+
+struct CustomToggleStyle: ToggleStyle {
+    var onColor = Color.green
+    var offColor = Color.gray
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Button {
+            configuration.isOn.toggle()
+        } label: {
+            RoundedRectangle(cornerRadius: 16.0, style: .circular)
+                .strokeBorder(.clear, lineWidth: 1.0)
+                .background(
+                    RoundedRectangle(cornerRadius: 16.0, style: .circular)
+                        .fill(configuration.isOn ? onColor : offColor)
+                )
+                .frame(width: 50, height: 30)
+                .overlay(
+                    circle(configuration.$isOn)
+                )
+        }
+    }
+    
+    func circle(_ isOn: Binding<Bool>) -> some View {
+        Circle()
+            .fill(isOn.wrappedValue ? .white : .white.opacity(0.7))
+            .shadow(radius: 1, x: 0, y: 1)
+            .padding(1.5)
+            .offset(x: isOn.wrappedValue ? 10: -10)
+    }
+}
